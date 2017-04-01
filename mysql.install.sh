02c74f159c843df5031dc2ef5e38b881d4a87413
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2017-04-01 18:55:46 +0300

    mysql password fix

--- mysql.install.sh
+++ mysql.install.sh
@@ -2,8 +2,8 @@
 
 mysql_pass=X!vctDnCJ,YK_3Y}
 
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password ${mysql_pass}'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password ${mysql_pass}'
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $mysql_pass"
+sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $mysql_pass"
 apt-get -y install mysql-server
 
 #mysql_secure_installation
