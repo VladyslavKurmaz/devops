fc65c13ed0518b5f26dbc53ebcacf31da99f02b3
VladyslavKurmaz <vladislav.kurmaz@gmail.com>
2017-04-01 20:25:51 +0300

    sonarqube small fixes

--- linux.update.sh
+++ linux.update.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 apt-get -y update
 apt-get -y upgrade
+apt-get install -y mc
--- sonarqube.install.sh
+++ sonarqube.install.sh
@@ -12,8 +12,13 @@ rm /opt/sonarqube-$sonar/conf/sonar.properties
 mv /opt/sonarqube-$sonar/conf/sonar.properties.new /opt/sonarqube-$sonar/conf/sonar.properties
 
 
-mysql -u root < ./sonarqube.sql
+mysql -u root -p < ./sonarqube.sql
 
-#sonar.jdbc.url=jdbc:mysql://localhost:3306/sonar?useUnicode=true&characterEncoding=utf8&rewriteBatchedStatements=true&useConfigs=maxPerformance&useSSL=false
+## Uncomment/define
+#sonar.jdbc.username=
+#sonar.jdbc.password=
+#sonar.jdbc.url= ?useSSL=false
+
+## Run sonar
 #/opt/sonarqube-$sonar/bin/linux-x86-64/sonar.sh start
 
